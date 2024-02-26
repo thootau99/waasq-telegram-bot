800c54a04d04525009a3e6f905fd4da95e714e65
thootau <burkekhoo@gmail.com>
2024-02-26 23:32:22 +0900

    [Changed] dockerfile update

--- machine_reader.py
+++ machine_reader.py
@@ -3,7 +3,7 @@ import json
 
 class MachineReader: 
   def get_all_machines(self):
-    f = open('./devices.json')
+    f = open('/devices/devices.json')
     data = json.load(f)
     f.close()
     return data
